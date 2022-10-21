d5558c1189f992acbcea0dae01e1e550d4a9ca2e
saul.burgos <saul.burgos@ooqia.com>
2022-10-21 16:16:24 -0600

    header and cookies

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from doctest import Example
 from typing import Optional
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from fastapi import FastAPI, status
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 
 app = FastAPI()
 
@@ -126,3 +126,27 @@ def login(
     password: str = Form(...)
 ):
     return LoginOut(username=username, message="logout successfuly")
+
+
+#Cookies and headers
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(..., min_length=1),
+    user_agent: Optional[str] = Header(default=None),
+    cookie: Optional[str] = Cookie(default=None)
+):
+    return user_agent
